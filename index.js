5493a9b00b94032af44f1822cc3d49c0bc42a5ec
Nate Wallace <natewallace@gmail.com>
2016-01-21 14:39:45 -0500

    Added react rules.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
   'extends': [
-    'eslint-config-iservices/base'
+    'eslint-config-iservices/base',
+    'eslint-config-iservices/rules/strict',
+    'eslint-config-iservices/rules/react'
   ].map(require.resolve),
   rules: {}
 };
